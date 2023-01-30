9c02682b8f5ebf92876bd2597b41303684c3623c
LiamSmith1992 <liamj.smith@hotmail.com>
2023-01-29 19:10:21 -0700

    getting some front end done

--- keeper.client/src/pages/AccountPage.vue
+++ keeper.client/src/pages/AccountPage.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
+    <section class="row cover-img m-2 rounded">
+      <div class="col-11 d-flex text-center">
+
+
+      </div>
+    </section>
+
+
+    <img class="rounded-pill" :src="account.picture" alt="" />
+    <h1>{{ account.name }}</h1>
     <p>{{ account.email }}</p>
   </div>
+  <p>{{ vaults. }}</p>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { AppState } from '../AppState'
+import { useRoute } from "vue-router"
+import { accountService } from "../services/AccountService"
+import { logger } from "../utils/Logger"
+import { vaultsService } from "../services/VaultsService"
+import { keepsService } from "../services/KeepsService"
 export default {
+
+
   setup() {
+    onMounted(() => {
+      getAccountVaults()
+      getAccountKeeps()
+    })
+    const route = useRoute()
+
+    async function getAccountVaults() {
+      try {
+        await vaultsService.getAccountVaults()
+      } catch (error) {
+        logger.error(error.message)
+      }
+    }
+
+    async function getAccountKeeps() {
+      try {
+        debugger
+        await keepsService.getProfileKeeps(route.params.id)
+
+      } catch (error) {
+        logger.error(error.message)
+      }
+    }
+
+
+
+
     return {
-      account: computed(() => AppState.account)
+      account: computed(() => AppState.account),
+      coverImg: computed(() => `url(${AppState.account?.coverImg})`),
+      vaults: computed(() => AppState.vaults),
+
     }
   }
 }
@@ -21,5 +67,14 @@ export default {
 <style scoped>
 img {
   max-width: 100px;
+}
+
+.cover-img {
+  height: 40vh;
+  background-image: v-bind(coverImg);
+  object-fit: cover;
+  width: 1;
+  border: solid 3px black;
+
 }
 </style>
